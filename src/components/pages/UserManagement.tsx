b73f61d0b0c0eb4129a74748f0bec6a76632451d
stogashi146 <stogashi146@gmail.com>
2022-06-23 09:30:16 +0900

    ユーザー一覧取得

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,19 @@
-import { memo, FC } from "react"
+import { memo, FC } from "react";
+import { Box, Image, Wrap, Stack, Text, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "./organisms/user/UserCard";
+import { useAllUsers } from "../hooks/useAllUsers";
 
 export const UserManagement: FC = memo(() => {
-  return <p>ユーザー設定ページ</p>
+  const { getUsers, users, loading } = useAllUsers();
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl={"https://source.unsplash.com/random"}
+          userName={"じゃけえ"}
+          fullName={"Takumi Okada"}
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
